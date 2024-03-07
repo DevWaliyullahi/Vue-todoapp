5f7c3ec849fe92406331cc1e6e6f0a6a12c5ee29
Waliyullahi <olalekanwaliyullahi99@gmail.com>
2024-03-07 14:07:28 +0100

    vue

--- todo-app/src/components/TodoItem.vue
+++ todo-app/src/components/TodoItem.vue
@@ -5,31 +5,32 @@
       <button @click="removeTodo">Remove</button>
     </div>
   </template>
-  
-  <script setup lang="ts">
-  import { defineProps, defineEmits } from 'vue';
-  
-  const props = defineProps<{
-    todo: {
-      id: number;
-      text: string;
-      completed: boolean;
-    };
-  }>();
-  
-  const { emit }  = defineEmits(['toggle', 'remove']);
-  
-  const toggleCompletion = () => {
-    emit('toggle', props.todo.id);
-  };
-  
-  const removeTodo = () => {
-    emit('remove', props.todo.id);
-  };
-  </script>
-  
-  <style scoped>
-    @import '../style.css';
+<script setup lang="ts">
 
-  </style>
-  
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  todo: {
+    id: number;
+    text: string;
+    completed: boolean;
+  };
+}>();
+
+// Use type assertion to specify the type of emit
+const { emit } = defineEmits(['toggle', 'remove']) as {
+  (event: 'toggle', id: number): void;
+  (event: 'remove', id: number): void;
+};
+
+const toggleCompletion = () => {
+  emit('toggle', props.todo.id);
+};
+
+const removeTodo = () => {
+  emit('remove', props.todo.id);
+};
+</script>
+<style scoped>
+@import '../style.css';
+</style>
